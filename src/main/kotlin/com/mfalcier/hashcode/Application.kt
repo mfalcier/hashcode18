ff421c8c5c2cb22799539ff791d06d81c6531d6a
mfalcier <marcofalcier@gmail.com>
2018-02-28 09:55:04 +0100

    :space_invader: Added comments

--- src/main/kotlin/com/mfalcier/hashcode/Application.kt
+++ src/main/kotlin/com/mfalcier/hashcode/Application.kt
@@ -3,6 +3,9 @@ package com.mfalcier.hashcode
 import java.io.File
 import java.io.InputStream
 
+/**
+ * Main function, passing a path and the filename
+ */
 fun main(args : Array<String>) {
     val startingTime = System.currentTimeMillis()
     if (args.isEmpty()) {
@@ -17,11 +20,14 @@ fun main(args : Array<String>) {
     val elaboratedMatrix = executeCommand { elaborateMatrix(matrix) }
 
     print("Writing The Matrix... ")
-    executeCommand { convertResultIntoText(elaboratedMatrix, args[0], args[1].split(".")[0]) }
+    executeCommand { convertResultIntoFile(elaboratedMatrix, args[0], args[1].split(".")[0]) }
 
     println("Everything done in ${System.currentTimeMillis()-startingTime}ms.")
 }
 
+/**
+ * Given a function, it will execute it and print the execution time
+ */
 fun <T> executeCommand(foo: () -> T): T {
     val startingTime = System.currentTimeMillis()
     val result = foo()
@@ -29,6 +35,9 @@ fun <T> executeCommand(foo: () -> T): T {
     return result
 }
 
+/**
+ * Given a path and a filename, it will convert it into a matrix
+ */
 fun convertFileIntoMatrix(path: String, file: String): MutableList<MutableList<String>> {
     val superMatrix = mutableListOf<MutableList<String>>()
 
@@ -44,12 +53,19 @@ fun convertFileIntoMatrix(path: String, file: String): MutableList<MutableList<S
     return superMatrix
 }
 
+
+/**
+ * Given a matrix, it will process it and return another matrix
+ */
 fun elaborateMatrix(matrix: MutableList<MutableList<String>>): MutableList<MutableList<String>> {
     matrix.removeAt(0)
     return matrix
 }
 
-fun convertResultIntoText(matrix: MutableList<MutableList<String>>, path: String, file: String) {
+/**
+ * Given a matrix, a path and a filename, it will print it into a file
+ */
+fun convertResultIntoFile(matrix: MutableList<MutableList<String>>, path: String, file: String) {
     val output = File("${path}output/$file.out")
     for (row in matrix) {
         var stringRow = ""
